724e0228e55bcb0bd55e6233cd34fa52b67ce441
Gakuto1112 <gakuto131112@gmail.com>
2022-03-11 16:28:38 +0900

    日付時刻に応じて背景色変更処理実装

--- ColorCodeClock.js
+++ ColorCodeClock.js
@@ -1,19 +1,46 @@
+function zeroFillTwoDigit(value) {
+	//0字詰めして2桁にして返す。
+	return ("0" + value).slice(-2);
+}
+
+function RGBParse(RGBString) {
+	//「rgb(0, 0, 0)」の形式。形式に沿わないものはnullを返す。
+	if(/rgb\(\d{1,3}, \d{1,3}, \d{1,3}\)/.test(RGBString)) return RGBString.match(/\d{1,3}/g).map((valueString) => Number(valueString));
+	else return null
+}
+
 setTimeout(() => {
-	const dayName = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", 'Fri.', 'Sat.'];
+	const weekdayName = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", 'Fri.', 'Sat.'];
 	const year = document.getElementById("year");
 	const month = document.getElementById("month");
-	const date = document.getElementById("date");
 	const day = document.getElementById("day");
+	const weekday = document.getElementById("weekday");
 	const hour = document.getElementById("hour");
 	const minute = document.getElementById("minute");
+	const date = document.getElementById("date");
+	const time = document.getElementById("time");
 	setInterval(() => {
 		const dateTime = new Date();
 		if(year.innerText == "--" || Number(year.innerText) != dateTime.getFullYear() % 100) year.innerText = dateTime.getFullYear() % 100;
-		if(month.innerText == "--" || Number(month.innerText) != dateTime.getMonth() + 1) month.innerText = ("0" + (dateTime.getMonth() + 1)).slice(-2);
-		if(date.innerText == "--" || Number(date.innerText) != dateTime.getDate()) date.innerText = ("0" + dateTime.getDate()).slice(-2);
-		if(day.innerText == "---" || dayName.indexOf(day.innerText) != dateTime.getDay()) day.innerText = dayName[dateTime.getDay()];
-		if(hour.innerText == "--" || Number(hour.innerText) != dateTime.getHours()) hour.innerText = ("0" + dateTime.getHours()).slice(-2);
-		if(minute.innerText == "--" || Number(minute.innerText) != dateTime.getMinutes()) minute.innerText = ("0" + dateTime.getMinutes()).slice(-2);
-		document.getElementById("second").innerText = ("0" + dateTime.getSeconds()).slice(-2);
+		if(month.innerText == "--" || Number(month.innerText) != dateTime.getMonth() + 1) month.innerText = zeroFillTwoDigit(dateTime.getMonth() + 1);
+		if(day.innerText == "--" || Number(day.innerText) != dateTime.getDate()) day.innerText = zeroFillTwoDigit(dateTime.getDate());
+		if(weekday.innerText == "---" || weekdayName.indexOf(weekday.innerText) != dateTime.getDay()) weekday.innerText = weekdayName[dateTime.getDay()];
+		if(hour.innerText == "--" || Number(hour.innerText) != dateTime.getHours()) hour.innerText = zeroFillTwoDigit(dateTime.getHours());
+		if(minute.innerText == "--" || Number(minute.innerText) != dateTime.getMinutes()) minute.innerText = zeroFillTwoDigit(dateTime.getMinutes());
+		document.getElementById("second").innerText = zeroFillTwoDigit(dateTime.getSeconds());
+		if(date.style.backgroundColor != "rgb(" + parseInt(year.innerText, 16) + ", " + parseInt(month.innerText, 16) + ", " + parseInt(day.innerText, 16) + ")") {
+			date.style.backgroundColor = date.innerText.match(/#\d{6}/)[0];
+			if(RGBParse(date.style.backgroundColor).reduce((sum, value) => sum + value, 0) / 3 < 128) date.style.color = "white";
+			else date.style.color = "black";
+		}
+		document.body.style.backgroundColor = time.innerText;
+		if(RGBParse(document.body.style.backgroundColor).reduce((sum, value) => sum + value, 0) / 3 < 128) {
+			time.style.color = "white";
+			//TODO: アナログ時計の文字色変更
+		}
+		else {
+			time.style.color = "black";
+			//TODO: アナログ時計の文字色変更
+		}
 	}, 1000);
 }, 1000 - new Date().getMilliseconds);
